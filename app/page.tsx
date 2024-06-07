6f1db9a6cf01b1b88aeb96ea8619b2d34d94fd79
Avyukt Soni <soniavyukt@gmail.com>
2024-06-07 11:32:58 +0530

    project changes

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,6 @@ import Main from "../components/Main";
 import About from "../components/About";
 import Navbar from "../components/Navbar";
 import { Projects } from "@/components/Projects";
-import { Projects2 } from "@/components/Projects2";
 import Skills from "../components/Skills";
 import { Experiences } from "@/components/Experiences";
 import { Connect } from "@/components/Connect";
@@ -24,7 +23,6 @@ export default function Home() {
         <Experiences />
         <Skills />
         <Projects />
-        {/* <Projects2 /> */}
         <Connect />
         <Footer />
       </main>
