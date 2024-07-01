f1e53e47b115464908466f19c98e7040cf76efa5
Avyukt Soni <soniavyukt@gmail.com>
2024-07-02 00:46:14 +0530

    refactoring

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import Main from "../components/Main";
+// import Main from "../components/Main";
 import { Landing } from "@/components/landing";
 import About from "../components/About";
 import Navbar from "../components/Navbar";
