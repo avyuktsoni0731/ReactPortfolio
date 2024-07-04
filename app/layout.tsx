23071a857b21ff4b17804580f3b26743780b4f1d
Avyukt Soni <soniavyukt@gmail.com>
2024-07-04 21:53:35 +0530

    favicon changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,12 +2,17 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Head from "next/head";
+import Favicon from "./favicon.ico";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Avyukt Soni | Portfolio",
   description: "Avyukt's Portfolio Website",
+  // icons: {
+  //   icon: "/AvyuktFavicon.png",
+  // },
+  icons: [{ rel: "icon", url: Favicon.src }],
 };
 
 export default function RootLayout({
